59d6511c75899cbde2af74ebb39c65ad7955e133
simonemurzilli <simone.murzilli@fao.org>
2016-03-21 11:54:55 +0100

    updated i18n

--- nls/fr/translate.js
+++ nls/fr/translate.js
@@ -1,3 +1,6 @@
+/*global define*/
 define({
+
+    courtesy: 'There is no data available for the selected FAOSTAT Domain.<br>We apologize for any inconvenience.'
 
 });
--- nls/translate.js
+++ nls/translate.js
@@ -2,7 +2,7 @@
 define({
     root : {
         error: 'Error',
-        courtesy: 'There is no data available for the selected FAOSTAT code.<br>We apologize for any inconvenience.'
+        courtesy: 'There is no data available for the selected FAOSTAT Domain.<br>We apologize for any inconvenience.'
     },
     fr : true,
     es : true
